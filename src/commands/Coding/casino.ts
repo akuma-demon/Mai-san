f9141a20215d33d1caed5360cd2ccba2eb0091ce
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-09 12:12:25 +0530

    Create casino.ts

---
+++ src/commands/Coding/casino.ts
@@ -0,0 +1,37 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'casino',
+            aliases: ['support,casino'],
+            description: 'Gets the group links',
+            category: 'extras',
+            usage: `${client.config.prefix}casino`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        (await this.client.sendMessage(
+        M.sender.jid,
+                `*🐰Casino Group🐰*\n\n
+                 *🐰𝑀𝒶𝒾 𝓈𝒶𝓃: Fun Group:https://chat.whatsapp.com/EeIT4nf7PBUD8Kwbm4FgJC\n\n
+                 *🐰𝑀𝒶𝒾 𝓈𝒶𝓃 𝒢𝒶𝓂𝒷𝓁𝒾𝓃𝑔: 𝗖𝗮𝘀𝗶𝗻𝗼 *:https://chat.whatsapp.com/EeIT4nf7PBUD8Kwbm4FgJC `,
+           MessageType.text
+        ))
+        const n = [
+            'https://c.tenor.com/DEBVzQIAjWwAAAPo/mai-sakurajima.mp4'
+        ]
+        let rin = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `Send you the support group links in Personal Message\n` }
+        )
+
+        }
+}
