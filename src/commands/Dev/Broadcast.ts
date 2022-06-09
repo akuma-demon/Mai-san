7b90faeb1ea8cefaa7b5104036cc4f15c1adc56f
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-09 12:39:43 +0530

    Create Broadcast.ts

---
+++ src/commands/Dev/Broadcast.ts
@@ -0,0 +1,55 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "broadcast",
+			description:
+				"Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
+			aliases: ["bcast", "announcement", "bc"],
+			category: "dev",
+			dm: true,
+			usage: `${client.config.prefix}bc`,
+			modsOnly: true,
+			baseXp: 0,
+		});
+	}
+
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		if (!joined)
+			return void (await M.reply(`Please provide the Broadcast Message.`));
+		const term = joined.trim();
+		const gifs = [
+			"https://c.tenor.com/DRn0g0kqoLAAAAPo/mai-sakurajima-anime.mp4",
+			"https://c.tenor.com/6j-sSlVmAVsAAAPo/mai-sakurajima.mp4",
+		];
+		const selected = gifs[Math.floor(Math.random() * gifs.length)];
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const chats: any = this.client.chats
+			.all()
+			.filter((v) => !v.read_only && !v.archive)
+			.map((v) => v.jid)
+			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.filter((v) => v);
+		for (let i = 0; i < chats.length; i++) {
+			const text = `*🐰「𝑀𝒶𝒾 𝓈𝒶𝓃 𝐵𝓇𝑜𝒶𝒹𝒸𝒶𝓈𝓉」🐰*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
+				mimetype: Mimetype.gif,
+				caption: `${text}`,
+				contextInfo: {
+					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
+				},
+			});
+		}
+		await M.reply(`🐰 Broadcast Message sent to *${chats.length} groups*.`);
+	};
+}
