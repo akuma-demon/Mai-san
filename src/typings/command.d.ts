a98230d8c69042d64d38f8b9d56f72470154f4d7
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-09 18:21:19 +0530

    Create command.d.ts

---
+++ src/typings/command.d.ts
@@ -0,0 +1,44 @@
+import MessageHandler from "../Handlers/MessageHandler";
+import { WAClient } from "../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "./";
+export interface ICommand {
+  client?: WAClient;
+  handler?: MessageHandler;
+  run(
+    M: ISimplifiedMessage,
+    parsedArgs: IParsedArgs
+  ): Promise<void | never> | void | never;
+  config: {
+    adminOnly?: boolean;
+    aliases?: string[];
+    description?: string;
+    command: string;
+    id?: string;
+    category?: TCategory;
+    usage?: string;
+    dm?: boolean;
+    baseXp?: number;
+    modsOnly?: boolean;
+  };
+}
+
+export type TCategory =
+  | "characters"
+  | "coding"
+  | "creation"
+  | "dev"
+  | "economy"
+  | "fun"
+  | "games"
+  | "extras"
+  | "educative"
+  | "general"
+  | "media"
+  | "moderation"
+  | "music"
+  | "youtube"
+  | "pokemon"
+  | "nsfw"
+  | "utils"
+  | "weeb"
+  | "category";
